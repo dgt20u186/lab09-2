fc6bddb37e147657f3f213e338cf313318dcdad6
morsiiik <ognezvezd02@mail.ru>
2021-04-06 17:46:58 +0300

    gdfd

--- solver_application/equation.cpp
+++ solver_application/equation.cpp
@@ -18,12 +18,11 @@ int main()
     {
         solve(a, b, c, x1, x2);
 
-        formatter(std::cout, std::string("x1 = 0"));
-        formatter(std::cout, std::string("x2 = 0"));
+        
     }
     catch (const std::logic_error& ex)
     {
-        formatter(std::cout, ex.what());
+        
     }
 
     return 0;
