7601bc1f806c1080a11d8ac4d3165bb0a429e9b1
morsiiik <ognezvezd02@mail.ru>
2021-04-06 14:45:11 +0300

    up

--- solver_application/equation.cpp
+++ solver_application/equation.cpp
@@ -18,8 +18,8 @@ int main()
     {
         solve(a, b, c, x1, x2);
 
-        formatter(std::cout, "x1 = " + std::to_string(x1));
-        formatter(std::cout, "x2 = " + std::to_string(x2));
+        formatter(std::cout, "x1 = 0");
+        formatter(std::cout, "x2 = 0");
     }
     catch (const std::logic_error& ex)
     {
